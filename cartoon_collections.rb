be4a6dd45fe26fd736faf2a9d4dc24aacb9e17ff
Raul Sposito <raulsposito@gmail.com>
2019-07-24 23:39:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -33,7 +33,7 @@ def long_planeteer_calls(array)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(food)
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
