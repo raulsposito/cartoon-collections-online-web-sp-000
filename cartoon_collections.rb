96d93384ac37bb773f52482c48207f42c8819c47
Raul Sposito <raulsposito@gmail.com>
2019-07-24 23:37:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,8 +29,8 @@ def long_planeteer_calls(array)
   if array.any? {|i| i.length > 4}
     return true 
   else return false 
-    i += 1 
-  # Your code here
+    i = i + 1 
+  end
 end
 
 def find_the_cheese# code an argument here
